0186293d74f7a7c79f703be695b31489b393830a
Aziz rupawala Aziz rupawala <azizrupawala786@gmail.com>
2022-03-16 11:38:37 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,45 @@ import './App.css';
 import 'react-simple-keyboard/build/css/index.css';
 
 function App() {
+    const [cells, setCells] = useState(Array(30).fill(null));
     const keyboard = useRef();
+    const chance = useRef(0);
+    const alphabet = useRef(0);
     const onKeyPress = (button) => {
         console.log(button);
+        if(button !== '{bksp}' && button !== '{enter}'){
+            if(alphabet.current > 4)
+                return;
+            switch (chance.current) {
+                case 0:
+                    setCells(prevCells => {
+                        prevCells[alphabet.current] = button;
+                        return prevCells;
+                    });
+                    alphabet.current+=1;
+            }
+        }
     }
   return (
-      <div>
+      <div className = "root">
+            <div className = "grid">
+                {cells.map(cell => 
+                    <div className = "grid-cell">
+                        <div className = "grid-text">{cell}</div>
+                    </div>
+                  )}
+            </div>
             <Keyboard
                 keyboardRef = 
                 {r => (keyboard.current = r)}
                 onKeyPress = {onKeyPress}
+                layout = {{
+                    default:[
+                'Q W E R T Y U I O P',
+                'A S D F G H J K L',
+                '{enter} Z X C V B N M {bksp}',
+                    ]
+                }}
             />
       </div>
   );
