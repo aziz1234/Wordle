51cbb0b63f5b9ae5ac72671332903b2c84c20430
Aziz rupawala Aziz rupawala <azizrupawala786@gmail.com>
2022-03-22 07:09:27 +0000

    implemented the logic to check if user input matches with the Wordle and made some UI improvements

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,82 @@ import './App.css';
 import 'react-simple-keyboard/build/css/index.css';
 
 function App() {
-    const [cells, setCells] = useState(Array(30).fill(null));
+    
+    const [cells, setCells] = useState(new Array(6).fill("").map(e => Array(5).fill("")));
+    const [cellStatus, setCellStatus] = useState(new Array(6).fill("").map(e => Array(5).fill("")));
+    const wordle = useRef("hello");
     const keyboard = useRef();
     const chance = useRef(0);
     const alphabet = useRef(0);
+    
     const onKeyPress = (button) => {
-        console.log(button);
+        console.log();
         if(button !== '{bksp}' && button !== '{enter}'){
             if(alphabet.current > 4)
                 return;
-            switch (chance.current) {
-                case 0:
+            setCells(prevCells => {
+                prevCells[chance.current][alphabet.current] = button;
+                return [...prevCells];
+            });
+            alphabet.current+=1;
+        }
+        else if(button === '{enter}') {
+            if(alphabet.current > 4)
+                checkForMatch();
+            else
+                window.alert('Not enough Alphabets');
+        }
+        else if(button === '{bksp}') {
+            if(alphabet.current < 1)
+                return;
                     setCells(prevCells => {
-                        prevCells[alphabet.current] = button;
-                        return prevCells;
+                        prevCells[chance.current][alphabet.current-1] = '';
+                        return [...prevCells];
                     });
-                    alphabet.current+=1;
+                    alphabet.current-=1;
+        }
+    }
+
+    const checkForMatch = () => {
+        let answer = wordle.current;
+        let input = [...cells[chance.current]];
+        let newTiles = new Array(6).fill("");
+
+        for(let i=0; i<5; i++) {
+            if(input[i].toLowerCase() === wordle.current[i]) {
+                newTiles[i] = "perfect-match";
+                answer.replace(input[i].toLowerCase(),'');
             }
+        }
+         for(let i=0; i<5; i++) {
+             if(input[i].toLowerCase() !== wordle.current[i] && answer.includes(input[i].toLowerCase())) {
+                 newTiles[i]= "almost-match";
+                 answer.replace(input[i].toLowerCase(),'');
+             }
+         }
+
+        setCellStatus(prevCells => {
+            prevCells[chance.current] = newTiles;
+            return [...prevCells];
+        })
+
+        if(chance.current < 6) {
+            chance.current+=1;
+            alphabet.current = 0;
         }
     }
   return (
       <div className = "root">
             <div className = "grid">
-                {cells.map(cell => 
-                    <div className = "grid-cell">
-                        <div className = "grid-text">{cell}</div>
-                    </div>
-                  )}
+                {
+                    cells.map((row, rIndex) => row.map((cell, cIndex) =>{
+                            return (
+                            <div key ={`${rIndex}-${cIndex}`} className ="grid-cell" id = {cellStatus[rIndex][cIndex]}>
+                                <div className ="grid-text">{cell}</div>
+                            </div>)
+                        }
+                    ))
+                }
             </div>
             <Keyboard
                 keyboardRef = 
@@ -43,6 +92,13 @@ function App() {
                 '{enter} Z X C V B N M {bksp}',
                     ]
                 }}
+                theme={"hg-theme-default hg-layout-default myTheme"}
+                buttonTheme= {[
+                  {
+                    class: "keyboard-buttons",
+                    buttons: "Q W E R T Y U I O P A S D F G H J K L {enter} Z X C V B N M {bksp}"
+                  }
+                ]}
             />
       </div>
   );
