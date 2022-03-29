5b534852e56f3b97d7b6bc260fd8c4967c4466cd
Aziz rupawala Aziz rupawala <azizrupawala786@gmail.com>
2022-03-29 08:54:20 +0000

    Few more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, {useRef, useState} from 'react';
 import Keyboard from 'react-simple-keyboard';
+import {VALID_GUESSES} from './constants/validGuesses';
+import {WORDS} from './constants/wordlist';
 import './App.css';
 import 'react-simple-keyboard/build/css/index.css';
 
@@ -11,9 +13,11 @@ function App() {
     const keyboard = useRef();
     const chance = useRef(0);
     const alphabet = useRef(0);
+    const disableUserInput = useRef(false);
     
     const onKeyPress = (button) => {
-        console.log();
+        if(disableUserInput.current)
+            return
         if(button !== '{bksp}' && button !== '{enter}'){
             if(alphabet.current > 4)
                 return;
@@ -43,28 +47,27 @@ function App() {
     const checkForMatch = () => {
         let answer = wordle.current;
         let input = [...cells[chance.current]];
-        let newTiles = new Array(6).fill("");
+        let newTiles = new Array(6).fill("no-match");
 
         for(let i=0; i<5; i++) {
             if(input[i].toLowerCase() === wordle.current[i]) {
                 newTiles[i] = "perfect-match";
-                answer.replace(input[i].toLowerCase(),'');
+                answer = answer.replace(input[i].toLowerCase(),'');
             }
         }
 
-        if(!answer.length) {
-            setCellStatus(prevCells => {
-                prevCells[chance.current] = newTiles;
-                return [...prevCells];
-            });
-            window.alert('Magnificent !!');
-            return;
-        }
-        
+        // if(!answer.length) {
+        //     setCellStatus(prevCells => {
+        //         prevCells[chance.current] = newTiles;
+        //         return [...prevCells];
+        //     });
+        //     window.alert('Magnificent !!');
+        //     return;
+        // }
          for(let i=0; i<5; i++) {
              if(input[i].toLowerCase() !== wordle.current[i] && answer.includes(input[i].toLowerCase())) {
                  newTiles[i]= "almost-match";
-                 answer.replace(input[i].toLowerCase(),'');
+                 answer = answer.replace(input[i].toLowerCase(),'');
              }
          }
 
@@ -74,8 +77,20 @@ function App() {
         })
 
         if(chance.current < 6) {
+            let perfectMatches = newTiles.filter(tile => tile === "perfect-match");
             chance.current+=1;
             alphabet.current = 0;
+            if(perfectMatches.length === 5) {
+                disableUserInput.current = true;
+                window.alert('Magnificent !!');
+            }
+            else if(chance.current >= 6) {
+                disableUserInput.current = true;
+                window.alert(`All chances have been exhausted \n correct guess: ${wordle.current}`);
+            }
+        }
+        else {
+            disableUserInput.current = true;
         }
     }
   return (
